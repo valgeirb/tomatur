75813ef0d66670200b1873567f538f92190e9b5e
Valgeir Bjornsson <valgeirb@valka.is>
2021-01-28 18:10:02 +0000

    Basic pomodoro functionality

---
+++ src/composables/usePomodoro.js
@@ -0,0 +1,79 @@
+import { reactive, computed, toRefs } from "vue";
+import * as workerTimers from "worker-timers";
+import useSettings from "./useSettings";
+
+const { session, shortBreak, longBreak } = useSettings();
+
+const state = reactive({
+  mode: "pomodoro", // pomodoro, shortBreak, longBreak
+  pomodoros: 0,
+  session: session.value,
+  shortBreak: shortBreak.value,
+  longBreak: longBreak.value,
+  remainingTime: session.value * 60,
+  minutes: session.value,
+  seconds: 0,
+});
+
+export default function usePomodoro() {
+  let intervalId = null;
+
+  const start = () => {
+    if (state.mode === "pomodoro") {
+      state.pomodoros++;
+    }
+
+    intervalId = workerTimers.setInterval(() => {
+      state.remainingTime--;
+      if (state.remainingTime === 0) {
+        workerTimers.clearInterval(intervalId);
+
+        // Continuously switches between pomodoro mode and break mode (short/long)
+        switch (state.mode) {
+          case "pomodoro":
+            if (state.pomodoros === 4) {
+              state.pomodoros = 0;
+              switchMode("longBreak");
+            } else {
+              switchMode("shortBreak");
+            }
+            break;
+          default:
+            switchMode("pomodoro");
+        }
+        start();
+      }
+    }, 1000);
+  };
+
+  const stop = () => {
+    workerTimers.clearInterval(intervalId);
+    state.minutes = session.value;
+    state.seconds = 0;
+    state.pomodoros = 0;
+    state.mode = "pomodoro";
+    state.remainingTime = session.value * 60;
+  };
+
+  const switchMode = (mode) => {
+    state.mode = mode;
+    state.remainingTime = state[mode] * 60;
+  };
+
+  const clock = computed(() => {
+    state.minutes = Math.floor(state.remainingTime / 60);
+    state.seconds = state.remainingTime % 60;
+
+    const minutes = state.minutes < 10 ? `0${state.minutes}` : state.minutes;
+    const seconds = state.seconds < 10 ? `0${state.seconds}` : state.seconds;
+
+    return `${minutes}:${seconds}`;
+  });
+
+  return {
+    ...toRefs(state),
+    start,
+    stop,
+    clock,
+  };
+}
--- src/views/Pomodoro.vue
+++ src/views/Pomodoro.vue
@@ -2,26 +2,17 @@
   <div class="h-full flex flex-col">
     <div class="flex-1 flex flex-col justify-center space-y-4">
       <div class="text-lg text-center">Start a new session</div>
-      <div class="text-6xl font-bold text-center w-full">{{ timeLeft }}</div>
+      <div class="text-6xl font-bold text-center w-full">{{ clock }}</div>
       <div class="text-lg hidden">until a small break</div>
     </div>
-    <BottomMenu @start="handleStart" @stop="handleStop" />
+    <BottomMenu @start="start" @stop="stop" />
   </div>
 </template>
 
 <script setup>
   import * as workerTimers from "worker-timers";
-  import { computed, ref } from "vue";
   import { BottomMenu } from "../components";
-  import usePomodoro from "../usePomodoro";
+  import usePomodoro from "../composables/usePomodoro";
 
-  const { start, stop, timeLeft } = usePomodoro();
-  const handleStart = () => {
-    console.log("Starting timer");
-    start(1);
-  };
-  const handleStop = () => {
-    console.log("Stopping timer");
-    stop();
-  };
+  const { start, stop, clock } = usePomodoro();
 </script>
