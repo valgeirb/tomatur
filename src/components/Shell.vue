a5d59d46dc8ecdb469206661f696588662904a78
Valgeir Bjornsson <valgeirb@valka.is>
2021-01-29 10:11:34 +0000

    Remove Squigglies

--- src/components/Shell.vue
+++ src/components/Shell.vue
@@ -1,13 +1,7 @@
 <template>
-  <Squigglies>
-    <div
-      class="select-none lg:select-auto relative overflow-auto h-full bg-white rounded-2xl p-4 shadow-2xl"
-    >
-      <slot />
-    </div>
-  </Squigglies>
+  <div
+    class="select-none lg:select-auto relative overflow-auto h-full bg-white p-8"
+  >
+    <slot />
+  </div>
 </template>
-
-<script setup>
-  import Squigglies from "./Squigglies.vue";
-</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export { default as BottomMenu } from "./BottomMenu.vue";
 export { default as Shell } from "./Shell.vue";
-export { default as Squigglies } from "./Squigglies.vue";
 export { default as TopMenu } from "./TopMenu.vue";
 export { default as Toggle } from "./Toggle.vue";
