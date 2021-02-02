1ed5445c01957cd85a9f12018e53bcfd187f53e3
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-02 21:24:37 +0000

    Add some transitions

--- src/views/Pomodoro.vue
+++ src/views/Pomodoro.vue
@@ -2,7 +2,7 @@
   <div class="h-full flex flex-col">
     <div class="relative flex-1 flex flex-col items-center justify-center">
       <Transition name="fade">
-        <ProgressRing
+        <Ring
           v-if="started"
           :started="started"
           :radius="150"
@@ -16,16 +16,35 @@
         <div class="text-lg hidden">until a small break</div>
       </div>
     </div>
-
-    <BottomMenu @start="start" @stop="stop" :started="started" />
+    <Controls
+      @start="start"
+      @stop="stop"
+      :started="started"
+      :showInfoButton="showInfoButton"
+    />
   </div>
+  <RouterView v-slot="{ Component }">
+    <Transition
+      class="absolute inset-0 bg-white dark:bg-gray-900 p-8"
+      enter-active-class="transform transition ease-in-out duration-300"
+      leave-active-class="transform transition ease-in-out duration-300"
+      enter-from-class="opacity-0 translate-y-full"
+      enter-to-class="opacity-100 translate-y-0"
+      leave-from-class="opacity-100 translate-y-0"
+      leave-to-class="opacity-0 translate-y-full"
+    >
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <script setup>
   import { computed } from "vue";
   import * as workerTimers from "worker-timers";
-  import { ProgressRing, BottomMenu } from "../components";
-  import usePomodoro from "../composables/usePomodoro";
+  import { Ring, Controls, Shell } from "../components";
+  import { usePomodoro, useSettings } from "../composables";
+
+  const { showInfoButton } = useSettings();
 
   const progress = computed(
     () => (remainingSeconds.value / initialRemainingSeconds.value) * 100,
