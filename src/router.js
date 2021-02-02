05b197a0f14d3adcd9b79f0ae102e60dc7d52709
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-02 14:48:57 +0000

    Add Settings as a child route

--- src/router.js
+++ src/router.js
@@ -2,9 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 import { Pomodoro, Settings, About, Sessions } from "./views";
 
 const routes = [
-  { path: "/", component: Pomodoro, name: "Pomodoro" },
+  {
+    path: "/",
+    component: Pomodoro,
+    name: "Pomodoro",
+    children: [
+      {
+        path: "/settings",
+        component: Settings,
+        name: "Settings",
+      },
+    ],
+  },
   { path: "/about", component: About, name: "About" },
-  { path: "/settings", component: Settings, name: "Settings" },
+
   { path: "/sessions", component: Sessions, name: "Sessions" },
 ];
 
