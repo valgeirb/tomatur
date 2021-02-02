ee8e70e127c99bb2f81100c7a4a8a64eb606df37
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-02 14:49:12 +0000

    TopMenu refactored and renamed

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="relative w-full flex flex-row items-center">
+    <RouterLink to="/" class="absolute left-0 text-2xl">
+      <FontAwesomeIcon icon="chevron-left" />
+    </RouterLink>
+    <div class="mx-auto flex items-center text-4xl font-bold uppercase">
+      <span>{{ title }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from "vue";
+
+  defineProps({
+    title: String,
+  });
+</script>
