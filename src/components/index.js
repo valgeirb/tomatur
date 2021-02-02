8c55d17bbc768022f539a284404e7d60dce89479
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-02 14:49:22 +0000

    Rename export

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as BottomMenu } from "./BottomMenu.vue";
 export { default as Shell } from "./Shell.vue";
-export { default as TopMenu } from "./TopMenu.vue";
+export { default as Header } from "./Header.vue";
 export { default as Toggle } from "./Toggle.vue";
 export { default as ProgressRing } from "./ProgressRing.vue";
