4ff61b95a8183cb8212c525f1e745b6424ba2ecf
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-02 14:49:47 +0000

    Hide overflow

--- src/components/Shell.vue
+++ src/components/Shell.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="select-none lg:select-auto relative overflow-auto h-full bg-white dark:bg-gray-900 text-black dark:text-gray-300 p-8"
+    class="select-none lg:select-auto relative overflow-hidden h-full bg-white dark:bg-gray-900 text-black dark:text-gray-300 p-8 lg:max-w-xl lg:mx-auto"
   >
     <slot />
   </div>
