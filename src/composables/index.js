55ed22023cc42dd00349be058342d10559d118c3
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-14 20:28:11 +0000

    Add composables to index

--- src/composables/index.js
+++ src/composables/index.js
@@ -1,2 +1,4 @@
 export { default as usePomodoro } from "./usePomodoro";
 export { default as useSettings } from "./useSettings";
+export { default as useSound } from "./useSound";
+export { default as useTheme } from "./useTheme";
