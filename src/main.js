011339c9c803a920dc90029b70da02233fd79f94
Valgeir Bjornsson <valgeirb@valka.is>
2021-02-14 20:27:22 +0000

    Tweak icons

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faSlidersH,
-  faQuestion,
+  faBars,
   faPlay,
   faStop,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
+import { faQuestionCircle } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import router from "./router";
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
 
-library.add(faSlidersH, faQuestion, faPlay, faStop, faChevronLeft);
+library.add(faBars, faQuestionCircle, faPlay, faStop, faChevronLeft);
 
 const app = createApp(App);
 app.component("FontAwesomeIcon", FontAwesomeIcon);
